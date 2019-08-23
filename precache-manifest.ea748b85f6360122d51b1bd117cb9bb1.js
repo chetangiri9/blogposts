d5536fb17e3c465f671482317d7bf5f6bb99b154
“chetangiri9” <chetangiri9@gmail.com>
2019-08-23 20:30:05 +0530

    Updates

---
+++ precache-manifest.ea748b85f6360122d51b1bd117cb9bb1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e44d6b3310c6bffa6e4c920d4d5d6bd",
+    "url": "/blogposts/index.html"
+  },
+  {
+    "revision": "cc416f1090aef6e65d12",
+    "url": "/blogposts/static/js/2.79222255.chunk.js"
+  },
+  {
+    "revision": "d928b0c709f51c43b54e",
+    "url": "/blogposts/static/js/main.d7bbfcea.chunk.js"
+  },
+  {
+    "revision": "2b2dfbb95c891cd7e264",
+    "url": "/blogposts/static/js/runtime~main.1c83242b.js"
+  }
+]);
